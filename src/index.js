aefa3b4a10b4ae9844f467890f2519934fcc47d0
Daniele <danielecamodeca@192.168.1.60>
2023-06-23 12:15:27 +0200

    api

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const modal = document.querySelector('.modal');
 const btnSearch = document.querySelector(".btn-primary");
 const loading = document.querySelector(".loading");
 const content = document.querySelector('.modal');
-const valueSearch = document.getElementById("form1");
+const valueSearch = document.getElementById("form1").value;
 
 //--->Eventi
 btnClose.addEventListener("click",close);
@@ -41,11 +41,44 @@ function close(){
     
 };
 
-function search(){
+function search() {
     loading.classList.remove("hidden");
+    const searchTerm = document.getElementById("form1").value;
+  
     setTimeout(function() {
-        loading.classList.add("hidden");
-        content.classList.remove("hidden");
-      }, 3000);
-
-}
+      loading.classList.add("hidden");
+      makeAPIRequest(searchTerm);
+      content.classList.remove("hidden");
+    }, 3000);
+  }
+  
+  function makeAPIRequest(searchTerm) {
+    const apiUrl = `https://openlibrary.org/search.json?q=${searchTerm}`;
+  
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        // Elabora i dati ottenuti dalla risposta
+        const results = data.docs; // Esempio: ottieni la lista dei risultati
+  
+        // Crea il markup HTML per i risultati
+        const resultsHTML = results.map(result => {
+          const title = result.title;
+          const author = result.author_name ? result.author_name[0] : 'Autore sconosciuto';
+  
+          return `<div class="result">
+                    <h3>${title}</h3>
+                    <p>Autore: ${author}</p>
+                  </div>`;
+        }).join('');
+  
+        // Aggiorna il contenuto con i risultati
+        const content = document.querySelector('.recipe-content');
+        content.innerHTML = `<div class="results-container">${resultsHTML}</div>`;
+      })
+      .catch(error => {
+        console.error(error);
+        // Gestisci eventuali errori
+      });
+  }
+  
