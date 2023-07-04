dd21b2572cdc9a37c930e2213ac72e5038446e31
Daniele <danielecamodeca@192.168.1.60>
2023-07-04 15:38:47 +0200

    ser

--- src/index.js
+++ src/index.js
@@ -1,160 +1,81 @@
-import axios from "axios";
 import './stile.css';
-const cors = require('cors');
+// Crea l'elemento footer
+const footer = document.createElement('footer');
+footer.textContent = 'Daniele Camodeca-© Copyright';
+
+// Ottieni il riferimento al contenitore dell'immagine e del footer
+const imageContainer = document.getElementById('image-container');
+const footerContainer = document.getElementById('footer-container');
+
+// Aggiungi l'immagine e il footer ai rispettivi contenitori
+
+footerContainer.appendChild(footer);
 
 
+//--->Seleziono elementi
+const btnClose = document.querySelector(".btn-close")
+const modal = document.querySelector('.modal');
+const btnSearch = document.querySelector(".btn-primary");
+const loading = document.querySelector(".loading");
+const content = document.querySelector('.modal');
+const valueSearch = document.getElementById("form1").value;
 
-const keyword = document.getElementById("keyword");
-const submit = document.getElementById("submit");
-const result = document.querySelector(".result");
-const categories = document.querySelector("nav");
-const categoriesList = document.querySelectorAll("nav li");
-const loader = document.querySelector(".loading");
-
-class Book {
-  constructor(title, cover, author, description) {
-    this.title = title;
-    this.cover = cover;
-    this.author = author;
-    this.description = description;
-  }
-}
-
-function draw(className, elToAppend, type) {
-  let newEl = document.createElement(type);
-  newEl.classList.add(className);
-  elToAppend.append(newEl);
-  return newEl;
-}
-
-async function callApi(subject) {
-  result.innerHTML = "";
-  loader.classList.remove("hidden");
-
-  try {
-    const response = await axios.get(
-      `https://openlibrary.org/subjects/${subject}.json`
-    );
-
-    if (response.data.work_count === 0) {
-      const message = draw("msg", result, "p");
-      message.innerHTML =
-        "Sorry! Nothing found, try to use different words...";
-      loader.classList.add("hidden");
-      return;
+//--->Eventi
+btnClose.addEventListener("click",close);
+btnSearch.addEventListener("click",search);
+window.addEventListener("keypress", function(event) {
+    // Verifica se il tasto premuto è il tasto Invio (codice 13)
+    if (event.keyCode === 13) {
+      search();
     }
+  });
 
-    const allTheBook = await Promise.all(
-      response.data.works.map(async (work) => {
-        return await createObjFromData(work);
+
+
+//--->Function
+function close(){
+    modal.classList.add('hidden');
+   
+};
+
+function search() {
+    loading.classList.remove("hidden");
+    const searchTerm = document.getElementById("form1").value;
+ 
+    setTimeout(function() {
+      loading.classList.add("hidden");
+      makeAPIRequest(searchTerm);
+      content.classList.remove("hidden");
+    }, 3000);
+  }
+ 
+  function makeAPIRequest(searchTerm) {
+    const apiUrl = `https://openlibrary.org/search.json?q=${searchTerm}`;
+ 
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        // Elabora i dati ottenuti dalla risposta
+        const results = data.docs; // Esempio: ottieni la lista dei risultati
+ 
+        // Crea il markup HTML per i risultati
+        const resultsHTML = results.map(result => {
+          const title = result.title;
+          const author = result.author_name ? result.author_name[0] : 'Autore sconosciuto';
+ 
+          return `<div class="result">
+                    <h3>${title}</h3>
+                    <p>Autore: ${author}</p>
+                  </div>`;
+        }).join('');
+ 
+        // Aggiorna il contenuto con i risultati
+        const content = document.querySelector('.recipe-content');
+        content.innerHTML = `<div class="results-container">${resultsHTML}</div>`;
       })
-    );
-
-    loader.classList.add("hidden");
-    renderBooks(allTheBook);
-  } catch (error) {
-    console.error(error.message);
-    alert(`Sorry something goes wrong, try again: ${error.message}`);
-  }
-}
-
-const getData = (e) => {
-  e.preventDefault();
-  const searchData = keyword.value.toLowerCase().trim().split(" ").join("_");
-  callApi(searchData);
-};
-
-const categoriesData = (e) => {
-  categoriesList.forEach((el) => el.classList.remove("active"));
-  if (e.target === categories) {
-    return;
-  }
-  e.target.classList.toggle("active");
-  const subject = e.target.dataset.categoria;
-  callApi(subject);
-};
-
-submit.addEventListener("click", getData);
-categories.addEventListener("click", categoriesData);
-
-async function createObjFromData(work) {
-  const book = new Book(work.title, "", "", "");
-  const res = await axios.get(`https://openlibrary.org${work.key}.json`);
-  const bookInfo = res.data;
-
-  const hasCover = bookInfo.covers && bookInfo.covers !== null;
-  book.cover = hasCover
-    ? `url(https://covers.openlibrary.org/b/id/${
-        bookInfo.covers.filter((n) => n != -1)[0]
-      }-M.jpg)`
-    : `url(/assets/Cover-not-found.png)`;
-
-  book.description =
-    bookInfo.description?.value ||
-    bookInfo.description ||
-    `Sorry! We don't have any description about this title`;
-
-  const hasAuthor = bookInfo.authors && bookInfo.authors !== null;
-  if (hasAuthor) {
-    const authorsRes = await axios.get(
-      `https://openlibrary.org${bookInfo.authors[0].author.key}.json`
-    );
-    book.author = authorsRes.data.name;
-  } else {
-    book.author = "No author found";
-  }
-  return book;
-}
-
-function renderBook(book) {
-  const cardBook = draw("card-book", result, "div");
-  const title = draw("title", cardBook, "h4");
-  const author = draw("author", cardBook, "h3");
-  const cover = draw("book-img", cardBook, "div");
-  title.innerText = book.title;
-  author.innerText = book.author;
-  cover.style.backgroundImage = book.cover;
-
-  cardBook.addEventListener("click", (e) => {
-    showModule(e, book);
-  });
-}
-
-function renderBooks(arrBook) {
-  arrBook.forEach((book) => renderBook(book));
-}
-
-function showModule(e, book) {
-  const thisBook = e.target;
-  const infoModule = draw("info", result, "div");
-  const closeBtn = draw("close", infoModule, "button");
-  const titleModule = draw("module-title", infoModule, "p");
-  const textModule = draw("module-text", infoModule, "p");
-  closeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="m6.062 14.708-.77-.77L9.229 10 5.292 6.062l.77-.77L10 9.229l3.938-3.937.77.77L10.771 10l3.937 3.938-.77.77L10 10.771Z"/></svg>`;
-  titleModule.innerHTML = "Description:";
-  textModule.innerHTML = book.description;
-
-  if (infoModule.scrollHeight > window.innerHeight) {
-    infoModule.style.maxHeight = "80vh";
-    infoModule.style.overflowY = "scroll";
+      .catch(error => {
+        console.error(error);
+        // Gestisci eventuali errori
+      });
   }
 
-  closeBtn.addEventListener("click", infoModule.remove);
-
-  document.addEventListener("click", (e) => {
-    const hasClickedOutsideTheModule =
-      e.target != infoModule &&
-      e.target != thisBook &&
-      e.target != titleModule &&
-      e.target != textModule;
-    if (hasClickedOutsideTheModule) {
-      infoModule.remove();
-    }
-  });
-
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-      infoModule.remove();
-    }
-  });
-}
